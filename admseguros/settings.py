0f62c5cc124af7c07fecb5040cac744dcaa2c3a8
nenecosistema <nenecosistema@gmail.com>
2023-09-03 22:32:36 -0300

    segunda

--- admseguros/settings.py
+++ admseguros/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static' , 
+    'C:/Users/eduardo/Documents/web/django/admseguros/seguros/static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
