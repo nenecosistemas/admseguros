3f194584aab3ef1f3739f6e53bd1fce1a7104693
Eduardo Recoliza <131217947+nenecosistemas@users.noreply.github.com>
2023-09-04 12:36:31 -0300

    Update models.py

--- seguros/models.py
+++ seguros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class compania(models.Model):
+  cuit_compania=models.CharField(primary_key=true,max_length=15)
+  nombre_compania=models.CharField(max_length=255) 
+  tipoiva_compania=models.PositiveSmallIntengerField() 
+  domicilio_compania=models.CharField(max_length=255) 
+  telefono_compania=models.CharField(max_length=45) 
+  correo_compania=models.Charfield(max_length=45)
